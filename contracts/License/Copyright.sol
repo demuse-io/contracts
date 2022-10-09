762cb116576e46caed1dafb74a3ef9a02562e6b3
migrenaa <mimonova13@gmail.com>
2022-10-09 00:38:00 -0500

    license & copyright

--- contracts/License/Copyright.sol
+++ contracts/License/Copyright.sol
@@ -3,15 +3,34 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Copyright is ERC721 {
-
+contract Copyright is IERC721, ERC721URIStorage {
+    event Minted(uint256 tokenId);
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    IERC721 songs;
+    mapping(uint256 => uint256) copyrightToSong;
 
-    constructor() ERC721("Copyright", "CPRT") {}
+    constructor(address songsAddr) ERC721("Copyright", "CPR") {
+        songs = IERC721(songsAddr);
+    }
 
-    function mint() public {
-       
+    function mint(string calldata metadata, uint256 songId)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
+
+        address owner = songs.ownerOf(songId);
+        _safeMint(owner, tokenId);
+        _setTokenURI(tokenId, metadata);
+
+        copyrightToSong[tokenId] = songId;
+
+        emit Minted(tokenId);
     }
 }
--- contracts/License/License.sol
+++ contracts/License/License.sol
@@ -1,19 +1,38 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract License is IERC721, ERC721URIStorage {
+    event Minted(uint256 tokenId);
 
-contract License is ERC721 {
-
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("License", "LCNS") {}
+    IERC721 copyrights;
+    mapping(uint => uint[]) copyrightToLicenses;
+    mapping(uint => uint) licenseToPrice;
 
-    function mint() public {
-       
+    constructor() ERC721("License", "LCNS") {
+        copyrights = IERC721(msg.sender);
+    }
+
+    function mint(uint copyrightId, uint price) public returns(uint) {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
+
+        address owner = copyrights.ownerOf(copyrightId);
+        _safeMint(owner, tokenId);
+
+        copyrightToLicenses[copyrightId].push(tokenId);
+        licenseToPrice[tokenId] = price;
+
+        emit Minted(tokenId);
+
+        return tokenId;
     }
 }
